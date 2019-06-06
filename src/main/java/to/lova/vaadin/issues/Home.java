7e9de39c668f6424742a150aebd59c5cf355f933
Giovanni Lovato <giovanni.lovato@axians.it>
2019-06-06 10:27:45 +0200

    Clean master to work in branches

--- src/main/java/to/lova/vaadin/issues/Home.java
+++
@@ -1,9 +0,0 @@
-package to.lova.vaadin.issues;
-
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
-
-@Route(value = "", layout = MainLayout.class)
-public class Home extends VerticalLayout {
-
-}
